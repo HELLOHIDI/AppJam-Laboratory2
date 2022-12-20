f202be75d2915ba2c96d5e87a18f06266bcc65e7
HELLOHIDI <fbgmlwo123@naver.com>
2022-12-20 12:39:47 +0900

    프로젝트 세팅

---
+++ AppJam_Laboratory2/AppJam_Laboratory2UITests/AppJam_Laboratory2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AppJam_Laboratory2UITestsLaunchTests.swift
+//  AppJam_Laboratory2UITests
+//
+//  Created by 류희재 on 2022/12/20.
+//
+
+import XCTest
+
+final class AppJam_Laboratory2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
