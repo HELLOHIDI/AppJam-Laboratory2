94b1f137689a7289f884e8b007e10c2bc79d5e27
HELLOHIDI <fbgmlwo123@naver.com>
2022-12-21 02:44:58 +0900

    [Feat] 환영뷰 AutoLayout 구현 #5

--- AppJam_Laboratory2/AppJam_Laboratory2/Screen /Welcome/WelcomeView.swift
+++ AppJam_Laboratory2/AppJam_Laboratory2/Screen /Welcome/WelcomeView.swift
@@ -21,13 +21,38 @@ final class WelcomeView: BaseView {
         $0.text = "000님 \n 환영합니다"
         $0.textColor = UIColor.black
         $0.textAlignment = .center
+        $0.numberOfLines = 2
     }
     
-    private var signUpButton = UIButton().then {
+    private var welcomeButton = UIButton().then {
         $0.backgroundColor = 0xFBE54C.color
         $0.setTitle("확인", for: .normal)
         $0.setTitleColor(0x000000.color, for: .normal)
         $0.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-Medium", size: 15)
         $0.titleLabel?.textAlignment = .center
+    }
+    
+    //MARK: - Custom Method
+    
+    override func setupView() {
+        [titleLabel, welcomeButton].forEach{
+            addSubview($0)
+        }
+    }
+    
+    override func setupConstraints() {
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaLayoutGuide).offset(197)
+            $0.leading.equalTo(self.safeAreaLayoutGuide).offset(144)
+            $0.trailing.equalTo(self.safeAreaLayoutGuide).offset(-144)
+        }
+        
+        welcomeButton.snp.makeConstraints {
+            $0.top.equalTo(self.titleLabel).offset(117)
+            $0.leading.trailing.equalTo(self.safeAreaLayoutGuide).inset(21)
+            $0.width.equalTo(333)
+            $0.height.equalTo(44)
+        }
     }
 }
