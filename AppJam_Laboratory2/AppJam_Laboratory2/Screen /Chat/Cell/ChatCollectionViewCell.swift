1d17b081269b48919ec4a2216cb28d031ec66175
HELLOHIDI <fbgmlwo123@naver.com>
2022-12-23 05:02:52 +0900

    [Feat] ChatCollectionViewCell 구현하기 #11

--- AppJam_Laboratory2/AppJam_Laboratory2/Screen /Chat/Cell/ChatCollectionViewCell.swift
+++ AppJam_Laboratory2/AppJam_Laboratory2/Screen /Chat/Cell/ChatCollectionViewCell.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import UIKit
+
+import SnapKit
+import SwiftyColor
+import Then
+
+final class ChatCollectionViewCell: UICollectionViewCell {
+    
+    //MARK: - Properties
+    
+    static let identifier = "ChatCollectionViewCell"
+    
+    //MARK: - UI Components
+
+    private let profileImageView = UIImageView().then {
+        $0.makeRounded(radius: 18)
+    }
+    
+    private let profileNameLabel = UILabel().then {
+        $0.font = UIFont(name: "AppleSDGothicNeo-SemiBold", size: 12)
+        $0.textColor = 0x191919.color
+    }
+    
+    private let profileMessageLabel = UILabel().then {
+        $0.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 11)
+        $0.textColor = 0xA6A6A6.color
+    }
+}
+
