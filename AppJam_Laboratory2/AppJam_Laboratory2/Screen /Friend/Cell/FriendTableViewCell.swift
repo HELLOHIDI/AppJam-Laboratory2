0412ecfefd057f41234244d6360f0e4278bb5f38
HELLOHIDI <fbgmlwo123@naver.com>
2022-12-22 14:17:16 +0900

    [Feat] FriendTableViewCell UI Components 구현 #9

---
+++ AppJam_Laboratory2/AppJam_Laboratory2/Screen /Friend/Cell/FriendTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  FriendTableViewCell.swift
+//  AppJam_Laboratory2
+//
+//  Created by 류희재 on 2022/12/22.
+//
+
+import Foundation
+import UIKit
+
+import SnapKit
+import SwiftyColor
+import Then
+
+final class FriendTableViewCell: UITableViewCell {
+    
+    //MARK: - Properties
+    static let identifier = "FriendTableViewCell"
+    
+    //MARK: - UI Components
+    private let profileImageView = UIImageView()
+    
+    private let profileNameLabel = UILabel().then {
+        $0.font = UIFont(name: "AppleSDGothicNeo-SemiBold", size: 12)
+        $0.textColor = 0x191919.color
+    }
+    
+    private let profileMessageLabel = UILabel().then {
+        $0.font = UIFont(name: "AppleSDGothicNeo-Normal", size: 11)
+        $0.textColor = 0xA6A6A6.color
+    }
+    
+}
