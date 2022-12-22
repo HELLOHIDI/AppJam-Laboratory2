6ce74e675137498f45cd5935afa11be5092872f2
HELLOHIDI <fbgmlwo123@naver.com>
2022-12-23 05:11:23 +0900

    [Feat] ChatCollectionViewCell AutoLayout 잡기 #11

--- AppJam_Laboratory2/AppJam_Laboratory2/Screen /Chat/Cell/ChatCollectionViewCell.swift
+++ AppJam_Laboratory2/AppJam_Laboratory2/Screen /Chat/Cell/ChatCollectionViewCell.swift
@@ -34,8 +34,52 @@ final class ChatCollectionViewCell: UICollectionViewCell {
         $0.textColor = 0xA6A6A6.color
     }
     
-    private let sendTime = UILabel().then {
+    private let sendTimeLabel = UILabel().then {
         $0.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 8)
+    }
+    
+    //MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Custom Method
+    
+    private func setupView() {
+        [profileImageView, profileNameLabel, profileMessageLabel, sendTimeLabel].forEach{
+            contentView.addSubview($0)
+        }
+    }
+    
+    private func setupConstraints() {
+        profileImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(4)
+            $0.leading.equalToSuperview().offset(16)
+            $0.width.equalTo(44)
+            $0.height.equalTo(41)
+        }
+        
+        profileNameLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(10)
+            $0.leading.equalTo(self.profileImageView.snp.trailing).offset(11)
+        }
+        
+        profileMessageLabel.snp.makeConstraints {
+            $0.top.equalTo(self.profileNameLabel.snp.bottom).offset(3)
+            $0.leading.equalTo(self.profileNameLabel)
+        }
+        
+        sendTimeLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(10)
+            $0.trailing.equalToSuperview().inset(16)
+        }
     }
 }
 
