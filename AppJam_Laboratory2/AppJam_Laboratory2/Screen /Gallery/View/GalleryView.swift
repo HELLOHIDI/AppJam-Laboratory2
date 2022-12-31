542a52d17b8d21893714e949e1f90c38c9227039
HELLOHIDI <fbgmlwo123@naver.com>
2022-12-31 17:56:35 +0900

    [Feat] 갤러리 컬렉션 뷰 구현 #15

--- AppJam_Laboratory2/AppJam_Laboratory2/Screen /Gallery/View/GalleryView.swift
+++ AppJam_Laboratory2/AppJam_Laboratory2/Screen /Gallery/View/GalleryView.swift
@@ -6,7 +6,43 @@
 //
 
 import Foundation
+import UIKit
+
+import SwiftyColor
+import Then
 
 final class GalleryView: BaseView {
     
+    private let galleryTopView = GalleryTopView()
+    
+    private let galleryCollectionView = UICollectionView(
+        frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()).then {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.isScrollEnabled = true
+            $0.showsVerticalScrollIndicator = false
+            $0.alwaysBounceVertical = true
+            // $0.delegate = self
+            // $0.dataSource = self
+        }
+    
+    override func setupView() {
+        [galleryTopView, galleryCollectionView].forEach {
+            addSubview($0)
+        }
+    }
+    
+    override func setupConstraints() {
+        galleryTopView.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(38)
+        }
+        
+        galleryCollectionView.snp.makeConstraints {
+            $0.top.equalTo(self.galleryTopView.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalToSuperview()
+        }
+    }
 }
+
